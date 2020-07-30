8fa0ccc62cd16dd1b6c83f79c3490049b6974f09
Luca Ruggieri <luca.ruggieri@packetai.co>
2020-07-30 15:02:13 +0200

    tests for processQuery added

--- main_test.go
+++ main_test.go
@@ -5,6 +5,12 @@ import (
 	"testing"
 )
 
-func TestFailCI(t *testing.T){
-	assert.Equal(t, 3, 1+1)
+func TestProcessQuery(t *testing.T){
+	assert.Equal(t, `{"data":{"jobs":[{"id":1},{"id":2},{"id":3},{"id":4},{"id":5}]}}`,processQuery("{jobs{id}}"))
+	assert.Equal(t, `{"data":{"companies":[{"id":1},{"id":2},{"id":3},{"id":4}]}}`,processQuery("{companies{id}}"))
+	assert.Equal(t, `{"data":{"people":[{"id":1},{"id":2},{"id":3},{"id":4}]}}`,processQuery("{people{id}}"))
+
+	assert.Equal(t, `{"data":{"job":{"position":"Software Engineer"}}}`,processQuery("{job(id:1){position}}"))
+
+	assert.Equal(t, `{"data":{"jobs":[{"company":{"id":1},"id":1},{"company":{"id":2},"id":2},{"company":{"id":3},"id":3},{"company":{"id":4},"id":4},{"company":{"id":4},"id":5}]}}`,processQuery("{jobs{id, company{id}}}"))
 }
